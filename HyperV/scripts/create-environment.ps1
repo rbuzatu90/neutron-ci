1fc22fd63d553d255bfa963d30685a870ba2cc51
Claudiu B <bclau@users.noreply.github.com>
2015-11-30 15:46:41 +0200

    Fixes typo from the previous commit

--- HyperV/scripts/create-environment.ps1
+++ HyperV/scripts/create-environment.ps1
@@ -204,7 +204,7 @@ ExecRetry {
     Write-Host "Doing fetch... refs/changes/18/250918/8"
     git fetch https://review.openstack.org/openstack/networking-hyperv refs/changes/18/250918/8
     Write-Host "Cherry-picking..  refs/changes/18/250918/8"
-    cherry-pick FETCH_HEAD
+    cherry_pick FETCH_HEAD
     & pip install C:\OpenStack\build\openstack\networking-hyperv
     if ($LastExitCode) { Throw "Failed to install networking-hyperv from repo" }
     popd
