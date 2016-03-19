18d517adbce66e99fdaf9570acfcf37acb804db5
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2016-03-19 19:52:24 +0200

    Removed pipefail option

--- jobs/run_job.sh
+++ jobs/run_job.sh
@@ -1,37 +1,31 @@
-    init
-    
-    use: /home/jenkins-slave/runs/devstack_params.$ZUUL_UUID.txt
-    run-tempest
-
-
-use: /home/jenkins-slave/runs/devstack_params.$ZUUL_UUID.txt
-collect
-
-
-
-
 #!/bin/bash
 jen_date=$(date +%d/%m/%Y-%H:%M)
 set +e
-set -o pipefail
-/usr/local/src/neutron-ci/jobs/run_initialize.sh 2>&1 | tee -a /home/jenkins-slave/logs/console-$ZUUL_UUID.log
+/usr/local/src/neutron-ci/jobs/run_initialize.sh 2>&1
 result_init=$?
-echo "$ZUUL_PROJECT;$ZUUL_BRANCH;$jen_date;$ZUUL_CHANGE;$ZUUL_PATCHSET;init;$result" >> /home/jenkins-slave/neutron-statistics.log
+echo "$ZUUL_PROJECT;$ZUUL_BRANCH;$jen_date;$ZUUL_CHANGE;$ZUUL_PATCHSET;init;$result_init" >> /home/jenkins-slave/neutron-statistics.log
+echo "Init job finished with exit code $result_init"
 
 if [ $result_init -eq 0 ]; then
     jen_date=$(date +%d/%m/%Y-%H:%M)
     source /home/jenkins-slave/runs/devstack_params.$ZUUL_UUID.txt
-    /usr/local/src/neutron-ci/jobs/run_tests.sh 2>&1 | tee -a /home/jenkins-slave/logs/console-$ZUUL_UUID.log
+    /usr/local/src/neutron-ci/jobs/run_tests.sh 2>&1
     result_tempest=$?
-    echo "$ZUUL_PROJECT;$ZUUL_BRANCH;$jen_date;$ZUUL_CHANGE;$ZUUL_PATCHSET;run;$result" >> /home/jenkins-slave/neutron-statistics.log
+    echo "$ZUUL_PROJECT;$ZUUL_BRANCH;$jen_date;$ZUUL_CHANGE;$ZUUL_PATCHSET;run;$result_tempest" >> /home/jenkins-slave/neutron-statistics.log
+    echo "Tempest job finished with exit code $result_tempest"
 fi
 
 jen_date=$(date +%d/%m/%Y-%H:%M)
 source /home/jenkins-slave/runs/devstack_params.$ZUUL_UUID.txt
 /usr/local/src/neutron-ci/jobs/run_collect.sh 
 result_collect=$?
+echo "Collect logs job finished with exit code $result_collect"
 
-if [  ]
+if [ $result_init -eq 0 ] && [ $result_tempest -eq 0 ]; then
+    $result = 0
+else
+    $result = 1
+fi
 
 set -e
 exit $result
