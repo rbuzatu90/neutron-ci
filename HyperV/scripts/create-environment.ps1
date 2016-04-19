6fc7c00fbc6378358ff6f93b146d9a617815723a
Remus Buzatu <r.buzatu90@gmail.com>
2016-04-19 08:37:12 +0300

    fix branch check for babel install

--- HyperV/scripts/create-environment.ps1
+++ HyperV/scripts/create-environment.ps1
@@ -192,7 +192,7 @@ Add-Content "$env:APPDATA\pip\pip.ini" $pip_conf_content
 & pip install cffi
 & pip install numpy
 
-if (($branchName.ToLower().CompareTo($('stable/mitaka').ToLower()) -or  $branchName.ToLower().CompareTo($('stable/liberty').ToLower()) -eq 0)) {
+if (($branchName.ToLower().CompareTo($('stable/mitaka').ToLower()) -eq 0) -or ($branchName.ToLower().CompareTo($('stable/liberty').ToLower()) -eq 0)) {
     & pip install Babel==2.2.0
 }
 
