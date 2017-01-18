7fe2e0302b6f5f2187e0d2156855f2c839adba50
Remus Buzatu <r.buzatu90@gmail.com>
2017-01-18 07:33:26 +0200

    Added missing variable
    Signed-off-by: Remus Buzatu <r.buzatu90@gmail.com>

--- devstack_vm/bin/collect_logs.sh
+++ devstack_vm/bin/collect_logs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-    DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+hyperv01=$1
+hyperv02=$2
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 . $DIR/config.sh
 . $DIR/utils.sh
 
