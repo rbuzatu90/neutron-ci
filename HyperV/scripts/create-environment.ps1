2f663a5b7d1757684b05d3ff11e926dc32667025
Remus Buzatu <r.buzatu90@gmail.com>
2017-01-20 09:55:36 +0200

    Apply os-win patch 422875/1 (#172)
    Signed-off-by: Remus Buzatu <r.buzatu90@gmail.com>

--- HyperV/scripts/create-environment.ps1
+++ HyperV/scripts/create-environment.ps1
@@ -314,6 +314,10 @@ if (@("stable/mitaka", "stable/newton", "master") -contains $branchName.ToLower(
         GitClonePull "$buildDir\os-win" "https://git.openstack.org/openstack/os-win.git" $branchName
         pushd $buildDir\os-win
 
+        # Allow setting VM snapshot types
+        git fetch git://git.openstack.org/openstack/os-win refs/changes/75/422875/1
+        cherry_pick FETCH_HEAD 
+
         & update-requirements.exe --source $buildDir\requirements .
         & pip install -U $buildDir\os-win
     }
